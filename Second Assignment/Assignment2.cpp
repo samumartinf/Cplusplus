2526fd28577054e1f5fd2d8386dbe9e5943ea053
samumartinf <samumartinfrias@gmail.com>
2017-11-16 11:09:21 +0000

    woking until the printing of the names in the first menu

--- Second Assignment/Assignment2.cpp
+++ Second Assignment/Assignment2.cpp
@@ -1,32 +1,58 @@
 #include <iostream>
-#include <cstdio>
-#include <string>
-#include <fstream>
+#include <sstream>
 #include <vector>
 
 using std::vector;
 using namespace std;
 
+//Classes
+class DNA_DB
+{
+  public:
+    string fileName;
+    DNA_DB(string fileName);
+};
+DNA_DB::DNA_DB(string fileName){
+  fileName = fileName;
+};
+
+//Functions used
+std::vector<DNA_DB> loadFiles(std::vector<DNA_DB> dna_db);
+void firstMenu(vector<DNA_DB> dna_db);
+
 int main(){
-  std::string fileNames;
+  vector<DNA_DB> dna_db;
+  dna_db.clear(); //Clear the vector before use
+  dna_db = loadFiles(dna_db);
+  firstMenu(dna_db);
+  return 0;
+}
+
+//Load files function: fills the vector<DNA_DB> with the names of the files
+vector<DNA_DB> loadFiles(vector<DNA_DB> dna_db){
+  string fileNames; //string to save the string with coma separated input
   cout << "DNA Sequence Database Software\nSpecify the name of DNA sequence file names you would like to load. For multiple files, add a \",\" between each file name. \n>";
-  getline(cin, fileNames);
-  // Variable for the loop
-  std::vector<char[]> fileNamesVector;
-  int startOfFileName = 0;
-  int numberOfFiles = 0;
-  for(int i = 0; i < fileNames.length(); i++) {
-    if (fileNames[i] == "," || fileNames[i] = "\n"){
-      fileNamesVector[numberOfFiles] = fileNames.substr(startOfFileName, i-1);
-      numberOfFiles++;
-      if (fileNames[i+1] != " ") /* Here we asume that the input will be separated by comas and no space
-      or by coma and a space (i.e. "file01, file02") but there will not be more than one space */
-        startOfFileName = i+1;
-      else
-        startOfFileName = i+2;
+  getline(cin,fileNames,'\n') ; //Get the whole input from the user and save it as fileNames
+  //cutting the string into separated names
+  istringstream ssFileNames(fileNames); //Creating a istringstream for the cutting of the sring
+  string tempString;
+  while(getline(ssFileNames, tempString, ',')) {
+    if (tempString[0] == ' '){
+      tempString = tempString.substr(1, tempString.size()-1);
     }
+    dna_db.push_back(DNA_DB(tempString));
   }
-  for (i = 0; i < numberOfFiles; i++){
-    print(fileNamesVector[i]);
+  return dna_db;
+}
+
+//Display and manage the data from the first menu
+void firstMenu(vector<DNA_DB> dna_db){
+  int i = 0;
+  std::cout << "Select one of the following options:" << '\n';
+  std::cout << "(S)  Summary of the statistics of the DNA database" << '\n';
+  while(i < dna_db.size()){
+    std::cout << '(' << i+1 << ")  Analyse" << dna_db[i].fileName << '\n'; //Debugging purposes
+    i++;
   }
+  std::cout << "(Q)  Quit" << '\n';
 }
